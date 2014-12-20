4f6570b517537670a2a082d67de5ebd6a1a9f45a
Sarah Walters <sarah.walters@students.olin.edu>
2014-12-20 12:09:00 -0500

    Adding postgres db infrastructure

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var express = require('express')
+var express = require('express');
 var app = express();
-var cool = require('cool-ascii-faces')
+var cool = require('cool-ascii-faces');
+var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -11,6 +12,20 @@ app.get('/', function(request, response) {
 	for (i=0; i<times; i++)
 		result += cool()
 	response.send(result);
+})
+
+app.get('/db', function(request, response) {
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		client.query('SELECT * FROM test_table', function(err, result) {
+			done();
+			if (err) {
+				console.error(err);
+				response.send("Error " + err);
+			} else
+				response.send(result.rows);
+			}
+		});
+	});
 })
 
 app.listen(app.get('port'), function() {
